fix bug: A is not symmetric due to softmax postprocess | trying better parameters

Model_MolGAN/evaluationGAN.py
@@ -88,8 +88,9 @@ def generateSample(size, draw=True):
     # Z-to-target
     edges_logits, nodes_logits = solver.G(z)
     (edges_hat) = solver.postprocess((edges_logits), solver.post_method)
+    edges_hat = (edges_hat + edges_hat.permute(1,0,2))/2
     A = torch.max(edges_hat, -1)[1]
-    rewardR = solver.evaluationReward(A, nodes_logits)[0][0]
+    rewardR = solver.reward(A, nodes_logits)[0][0]
     # print(nodes_logits.data.cpu().numpy())
     edges = A.data.cpu().numpy()
     edgeNums = np.sum(edges)/2.
@@ -170,7 +171,7 @@ def calEdgeNum(iter):
     times = 100
     solver = testCaseGenerator(iter)
     for i in tqdm(range(times)):
-        _, edgeNums, r = generateSample(20, draw=False)
+        _, edgeNums, r = generateSample(20, draw=True)
         totalNums += edgeNums
         totalRewards += r
         time.sleep(1)
@@ -227,7 +228,7 @@ def compareIteration():
         solver = testCaseGenerator(i*10000)
         totalReward = 0
         for j in range(times):
-            _, reward, _ = generateSample(15, draw=False)
+            _, _, reward = generateSample(15, draw=False)
             totalReward += reward
         aveReward = totalReward/times
         if aveReward >= maxReward:
@@ -239,5 +240,5 @@ def compareIteration():
     print("Max Iteration: ", maxIteration*10000, "   max Reward: ", maxReward)
 
 # compareIteration()
-calEdgeNum(90000)
-# calDistance(90000)
+calEdgeNum(200000)
+# calDistance(50000)

Model_MolGAN/rewardUtils.py
@@ -10,7 +10,9 @@ def calConnectivityRatio(A):
     for i in range(len(A)):
         G[i] = []
         for j in range(len(A[i])):
-            if A[i][j] == 1:
+            if i == j:
+                G[i].append(j)
+            elif A[i][j] == 1:
                 G[i].append(j)
     groups = tc(G)
     groups:dict
@@ -33,7 +35,9 @@ def evaConnectivityRatio(A):
     for i in range(len(A)):
         G[i] = []
         for j in range(len(A[i])):
-            if A[i][j] == 1:
+            if i == j:
+                G[i].append(j)
+            elif A[i][j] == 1:
                 G[i].append(j)
     groups = tc(G)
     groups:dict
@@ -41,7 +45,7 @@ def evaConnectivityRatio(A):
     for value in groups.values():
         difGroups.add(value)
     if len(difGroups) != 0:
-        return 1./len(difGroups)
+        return (1./len(difGroups))**(1/2)
     else:
         return 0.
 
@@ -50,34 +54,38 @@ def evaCalConnectivityReward(A):
     for i in range(len(A)):
         reward.append(evaConnectivityRatio(A[i]))
     return np.array(reward)
-    
-def calRedundancyRatio(A, X):
-    allLen = np.sum(A)
-    mstLen = len(X) - 1 # getMSTLenForReward(X)
-    if allLen != 0:
-        return 1 - abs((allLen - mstLen)/allLen)
-    else:
-        return 1
 
-def calRedundancyReward(A, X):
-    reward = []
-    for i in range(len(A)):
-        reward.append(calRedundancyRatio(A[i], X[i]))
-    return np.array(reward)
 
-A = [[[0,1,0,0,0],
-	[0,0,1,0,0],
-	[1,0,0,0,0],
-	[0,0,0,0,1],
-	[0,0,0,1,0]],
-    [[0,1,0,0,0],
-	[0,0,1,0,0],
-	[1,0,0,0,0],
-	[0,0,0,0,1],
-	[0,0,0,1,0]]]
+# A = [[
+#     [1,0,0,0,0],
+# 	[0,1,0,0,0],
+# 	[0,0,1,0,0],
+# 	[0,0,0,1,0],
+# 	[0,0,0,0,1]],
 
-X = [[[0,0], [0,1], [0,2], [1,3], [1,0.9]],
-    [[0,0], [0,1], [0,2], [1,3], [1,0.9]]]
+#     [[0,1,0,0,0],
+# 	[0,0,1,0,0],
+# 	[1,0,0,0,0],
+# 	[0,0,0,0,1],
+# 	[0,0,0,1,0]]]
 
-calConnectivityReward(A)
-calRedundancyReward(A, X)
+# X = [[[0,0], [0,1], [0,2], [1,3], [1,0.9]],
+#     [[0,0], [0,1], [0,2], [1,3], [1,0.9]]]
+
+# def testSubGraphNum(A):
+#     G = dict()
+#     for i in range(len(A)):
+#         G[i] = []
+#         for j in range(len(A[i])):
+#             if A[i][j] == 1:
+#                 G[i].append(j)
+#     groups = tc(G)
+#     groups:dict
+#     difGroups = set()
+#     for value in groups.values():
+#         difGroups.add(value)
+#     print(difGroups)
+#     return len(difGroups)
+
+# print(testSubGraphNum(A[0]))
+# calRedundancyReward(A, X)

Model_MolGAN/solver.py
@@ -11,7 +11,7 @@ from torchvision.utils import save_image
 from utilsGAN import *
 from models import Generator, Discriminator
 from dataGAN.sparse_molecular_dataset import SparseMolecularDataset
-from rewardUtils import calConnectivityReward, calRedundancyReward, evaCalConnectivityReward
+from rewardUtils import calConnectivityReward, evaCalConnectivityReward
 from tqdm import tqdm
 
 class Solver(object):
@@ -234,9 +234,9 @@ class Solver(object):
         if A_copy.ndim == 2:
             A_copy = [A_copy]
 
-        rr += 0.5*getTreeReward(A_copy, X_copy)
+        rr += 0.9*getTreeReward(A_copy, X_copy)
 
-        rr += 0.5*calConnectivityReward(A_copy)
+        rr += 0.1*evaCalConnectivityReward(A_copy)
         # rr *= calRedundancyReward(A.cpu().detach().numpy().copy() , X.cpu().detach().numpy().copy() )
         return rr.reshape(-1, 1)
 
@@ -248,8 +248,10 @@ class Solver(object):
             edgeNum = (np.sum(A)-traceSum)/ 2
             treeEdgeNum = len(X) - 1
             if edgeNum != 0:
-                rr *= 1 - abs((edgeNum - treeEdgeNum)/edgeNum)
+                rr *= 1 - ((edgeNum - treeEdgeNum)/edgeNum)**2
             else: 
+                rr = 0.
+            if rr < 0:
                 rr = 0.
             return rr
 
@@ -319,6 +321,8 @@ class Solver(object):
             edges_logits, nodes_logits = self.G(z)
             # Postprocess with Gumbel softmax
             (edges_hat) = self.postprocess((edges_logits), self.post_method)
+            # to be symetric
+            edges_hat = (edges_hat + edges_hat.permute(0,2,1,3))/2
             # nodes_hat = nodes_logits
             logits_fake, features_fake = self.D(edges_hat, None, nodes_logits)
             d_loss_fake = torch.mean(logits_fake)
@@ -361,6 +365,7 @@ class Solver(object):
                 rewardR = torch.from_numpy(self.reward(a, x)).to(self.device)
                 # Fake Reward
                 (edges_hard) = self.postprocess((edges_logits), self.post_method)
+                edges_hard = (edges_hard + edges_hard.permute(0,2,1,3))/2
                 edges_hard, nodes_hard = torch.max(edges_hard, -1)[1], nodes_logits
                 # mols = [self.data.matrices2mol(n_.data.cpu().numpy(), e_.data.cpu().numpy(), strict=True)
                 #         for e_, n_ in zip(edges_hard, nodes_hard)]
@@ -414,8 +419,8 @@ class Solver(object):
 
             # Decay learning rates.
             if (i+1) % self.lr_update_step == 0 and (i+1) > (self.num_iters - self.num_iters_decay):
-                g_lr -= (self.g_lr / float(self.num_iters_decay))
-                d_lr -= (self.d_lr / float(self.num_iters_decay))
+                g_lr -= (self.g_lr / 10) #float(self.num_iters_decay))
+                d_lr -= (self.d_lr / 10) #float(self.num_iters_decay))
                 self.update_lr(g_lr, d_lr)
                 print ('Decayed learning rates, g_lr: {}, d_lr: {}.'.format(g_lr, d_lr))
 
@@ -432,6 +437,7 @@ class Solver(object):
             edges_logits, nodes_logits = self.G(z)
             # Postprocess with Gumbel softmax
             (edges_hat, nodes_hat) = self.postprocess((edges_logits, nodes_logits), self.post_method)
+            edges_hat = (edges_hard + edges_hard.permute(0,2,1,3))/2
             A = torch.max(edges_hat, -1)[1]
             # print(A.data.cpu().numpy())
             # print(nodes_logits.data.cpu().numpy())